b18abfc632c823114607997c0d82013430d565d8
search3958 <157264638+search3958@users.noreply.github.com>
2026-06-17 21:29:28 +0900

    ついに認識

--- 32.sh
+++ 32.sh
@@ -3,7 +3,13 @@ set -e
 mkdir -p build32/EFI/BOOT
 cp limine-bin/BOOTIA32.EFI build32/EFI/BOOT/
 cp limine.conf build32/
+cp limine.conf build32/EFI/BOOT/
 echo "Compiling x86_32 kernel..."
 clang -target i386-unknown-windows -fuse-ld=lld -ffreestanding -fno-stack-protector -fshort-wchar -nostdlib -Wl,-entry:EfiMain -Wl,-subsystem:efi_application -Ikernel -o build32/kernel.efi kernel/main.c
 echo "Starting QEMU..."
-qemu-system-i386 -bios /opt/homebrew/share/qemu/edk2-i386-code.fd -drive file=fat:rw:build32,format=raw -m 512M -net none
+qemu-system-i386 \
+    -drive if=pflash,format=raw,readonly=on,file=/opt/homebrew/share/qemu/edk2-i386-code.fd \
+    -drive file=fat:rw:build32,format=raw \
+    -m 512M \
+    -vga std \
+    -net none
--- arm.sh
+++ arm.sh
@@ -3,7 +3,15 @@ set -e
 mkdir -p buildarm/EFI/BOOT
 cp limine-bin/BOOTAA64.EFI buildarm/EFI/BOOT/
 cp limine.conf buildarm/
+cp limine.conf buildarm/EFI/BOOT/
 echo "Compiling ARM64 kernel..."
 clang -target aarch64-unknown-windows -fuse-ld=lld -ffreestanding -fno-stack-protector -fshort-wchar -nostdlib -Wl,-entry:EfiMain -Wl,-subsystem:efi_application -Ikernel -o buildarm/kernel.efi kernel/main.c
 echo "Starting QEMU..."
-qemu-system-aarch64 -cpu cortex-a57 -M virt -bios /opt/homebrew/share/qemu/edk2-aarch64-code.fd -drive file=fat:rw:buildarm,format=raw -m 512M -device virtio-gpu-pci -device usb-ehci -device usb-mouse -device usb-kbd -net none
+qemu-system-aarch64 \
+    -cpu cortex-a57 -M virt \
+    -drive if=pflash,format=raw,readonly=on,file=/opt/homebrew/share/qemu/edk2-aarch64-code.fd \
+    -drive file=fat:rw:buildarm,format=raw \
+    -m 512M \
+    -device virtio-gpu-pci \
+    -device usb-ehci -device usb-mouse -device usb-kbd \
+    -net none
